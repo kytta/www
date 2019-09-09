127a7d0df5912f913a83ba6b950ef73c004c305f
Nikita Karamov <nick@karamoff.ru>
2019-09-09 16:54:53 +0300

    Switch to Handlebars

--- build/html.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const htmlInput = process.cwd() + "/src/html/index.html";
-const htmlOutput = process.cwd() + "/public/index.html";
-
-fs.createReadStream(htmlInput).pipe(fs.createWriteStream(htmlOutput));
