461911cb60b0df0f3c914d0a8b0a9946ef9c3f3b
Nikita Karamov <nick@karamoff.dev>
2020-06-07 02:01:20 +0200

    Restyle

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
     plugins: [
-        require('postcss-normalize')(),
-        require('postcss-preset-env')(),
         require('css-mqpacker')(),
         require('autoprefixer')(),
         require('cssnano')()
