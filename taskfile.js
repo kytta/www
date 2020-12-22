91f4efe392056834133c8e47ce93bee7dca44b8b
Nikita Karamov <nick@karamoff.dev>
2020-12-22 03:15:31 +0300

    Add 'vavilon' task

--- taskfile.js
+++ taskfile.js
@@ -6,7 +6,7 @@ const pug = require('pug');
 const OUTPUT_DIR = './public';
 
 exports.default = function * (task) {
-	yield task.parallel(['pug', 'static', 'styles']);
+	yield task.parallel(['pug', 'static', 'styles', 'vavilon']);
 }
 
 exports.styles = function * (task) {
@@ -53,4 +53,61 @@ exports.pug = function * (task) {
 
 exports.static = function * (task) {
 	yield task.source('./src/static/**/*').target(OUTPUT_DIR);
+}
+
+exports.vavilon = function * (task) {
+	const pugData = require('./src/pugData');
+	const dictionaries = {};
+
+	pugData.languages
+		.forEach(l => { dictionaries[l] = {} });
+
+	function translateSimpleString(key, langStringMap) {
+		function writeString(lang, key, val) {
+			dictionaries[lang][key] = val;
+		}
+
+		if (typeof langStringMap === 'string' || langStringMap instanceof String) {
+			Object.keys(dictionaries)
+				.forEach(lang => {
+					writeString(lang, key, langStringMap);
+				})
+		} else {
+			Object.entries(langStringMap)
+				.forEach(([lang, strOrArr]) => {
+					if (typeof strOrArr === 'string' || strOrArr instanceof String) {
+						writeString(lang, key, strOrArr);
+					} else if (strOrArr instanceof Array) {
+						for (let i = 0; i < strOrArr.length; i++) {
+							writeString(lang, `${key}-${i}`, strOrArr[i]);
+
+						}
+					}
+				});
+		}
+	}
+
+	["name", "descriptionMeta", "description"]
+		.forEach(k => translateSimpleString(k, pugData[k]));
+
+	pugData.socials.forEach(soc => {
+		translateSimpleString(soc.slug, soc.name);
+	});
+
+	pugData.listModules.forEach(mod => {
+		translateSimpleString(`m-${mod.slug}`, mod.title);
+		mod.items.forEach(item => {
+			translateSimpleString(`m-${mod.slug}-${item.slug}-name`, item.name);
+			translateSimpleString(`m-${mod.slug}-${item.slug}-desc`, item.description);
+		});
+	});
+
+	task._.files = Object.entries(dictionaries)
+		.map(([lang, dict]) => ({
+				dir: OUTPUT_DIR,
+				base:`${lang}.json`,
+				data: Buffer.from(JSON.stringify(dict))
+		}));
+
+	task.target(OUTPUT_DIR);
 }
