6791b8ed24bab0ee0fd3adf8e856e9032d521cfe
Nikita Karamov <me@kytta.dev>
2023-02-26 21:41:56 +0100

    New design of main + go up button

---
+++ static/lift.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", (_) => {
+	if (!matchMedia("screen and (min-width: 992px)").matches) {
+		return;
+	}
+
+	const MIN_SCROLL_Y = 100;
+	const MAX_SCROLL_Y = 500;
+	const MAX_OPACITY = 0.6;
+	const $goUp = document.getElementById("lift-go-up");
+
+	$goUp.addEventListener("click", (_) => {
+		window.scrollTo(0, 0);
+	});
+
+	const updateScroll = (_) => {
+		if (window.scrollY < MIN_SCROLL_Y) {
+			$goUp.style.display = "none";
+			return;
+		} else {
+			$goUp.style.display = "initial";
+		}
+
+		if (window.scrollY <= MAX_SCROLL_Y) {
+			$goUp.style.opacity =
+				(MAX_OPACITY * (window.scrollY - MIN_SCROLL_Y)) /
+				(MAX_SCROLL_Y - MIN_SCROLL_Y);
+			return;
+		}
+
+		$goUp.style.opacity = MAX_OPACITY;
+	};
+
+	document.addEventListener("scroll", updateScroll);
+	updateScroll();
+});
